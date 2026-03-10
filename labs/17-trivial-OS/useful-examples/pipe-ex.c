de06de62d90e7e7cfe4ed44437f05719af314880
engler <ddd.rrr.eee@gmail.com>
2026-03-09 18:03:04 -0700

    adding new

---
+++ labs/17-trivial-OS/useful-examples/pipe-ex.c
@@ -0,0 +1,83 @@
+// engler: Example of using pipe, fork, waitpid.  
+//
+// NOTE: writing to a closed pipe will terminate the writing process (SIGPIPE).
+// Read will return 0.   You can use variants of these facts to reliably
+// avoid some race conditions in detecting/dieing if your parent/child is dead 
+
+#include <assert.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#include "demand.h"
+
+// just echo what the child sends, make sure waitpid() can give us exit statis.
+void parent(int fd, int pid) {
+    // 1. Block til child dies, print exit code.  
+    // Q: 
+    //  - what non-determinism what can happen if we do (1) after (2) (below)
+	int status;
+	if(waitpid(pid, &status, 0) < 0)
+		sys_die(waitpid, should never fail);
+
+	trace("Parent done: ");
+    if(!WIFEXITED(status))
+        panic("Child crashed.\n");
+    else
+        trace("Child exited with status = %d\n", WEXITSTATUS(status));
+
+    // 2. Drain the pipe written by the user.
+    // - this is a weird order: could we (2) before (1)?
+	int ret;
+	char c;
+
+	// echo whever child sent.
+	trace("Parent [pid=%d]: echoing child output: <", getpid());
+	while((ret = read(fd, &c, 1)) == 1)
+        output("%c", c);
+	output(">\n");
+
+	// read() of closed pipe = EOF, which means ret == 0.  ret < 0
+	// is some other error.
+	if(ret < 0)
+		sys_die(read, returned an error);
+}
+
+void child(int fd) {
+	char buf[1024];
+	sprintf(buf, "Hello, child.getpid=[%d]", getpid());
+	if(write(fd, buf, strlen(buf)+1) < 0)
+		sys_die(write, child write failed);
+    trace("child: about to exit\n");
+	exit(10);
+}
+
+int main(void) {
+	int fds[2];
+	if(pipe(fds) < 0)
+		sys_die(pipe, cannot create pipe fds);
+
+    trace("about to fork parent\n");
+	int pid;
+	if((pid = fork()) < 0)
+		sys_die(fork, fork failed);
+
+	if(pid != 0) {
+        // can happen before or after parent print, so don't trace
+        output("parent: closing fds[1]\n");
+		if(close(fds[1]) < 0)
+			sys_die(close, impossible);
+		parent(fds[0], pid);
+	} else {
+        // can happen before or after child print, so don't trace
+        output("child: closing fds[0]\n");
+		if(close(fds[0]) < 0)
+			sys_die(close, close failed?);
+		child(fds[1]);
+	} 
+	return 0;
+}
