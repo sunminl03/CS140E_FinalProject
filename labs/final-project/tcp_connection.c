fef6b9dc16eb838ea1e3bc5e535ff79df9158bc1
Sunmin Lee <sunminlee@MacBook-Pro-4.local>
2026-03-14 01:54:00 -0700

    added test for tcp_receiver.c, started on tcp_connection.h/c

---
+++ labs/final-project/tcp_connection.c
@@ -0,0 +1,3 @@
+// TO DO. this code connects tcp segments and http/led
+// need to have state machines:TCP_CLOSED -> TCP_SYN_RCVD -> TCP_ESTABLISHED -> TCP_LAST_ACK
+// handle syn/ACK/FIN transitions correctly
